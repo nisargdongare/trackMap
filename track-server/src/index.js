3b3f738ffe5a6351effa5c217c7f375d9779713a
nisargdongare <drnfriend@gmail.com>
2020-07-08 02:32:20 +0530

    step1 for routes

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-// const mongoUri = 'mongodb+srv://admin:admin@cluster1.qc2mh.mongodb.net/test?retryWrites=true&w=majority';
 const mongoUri = 'mongodb://admin:admin@cluster1-shard-00-00.qc2mh.mongodb.net:27017,cluster1-shard-00-01.qc2mh.mongodb.net:27017,cluster1-shard-00-02.qc2mh.mongodb.net:27017/test?ssl=true&replicaSet=atlas-147g1s-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 mongoose.connect(mongoUri,{
@@ -14,6 +14,7 @@ mongoose.connection.on('error', (err)=>{console.error('mongoDB: connection error
 
 
 
+app.use(authRoutes);
 
 app.get('/',(req,res)=>{
     res.send('Hi there!');
